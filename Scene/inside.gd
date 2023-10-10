5942ff36b39ab1d43eb5edd5485e24057c102b70
The Empty <toangaming.203@gmail.com>
2023-10-11 04:01:46 +0700

    11/10 4:01, update finished 8h of the video

--- Scene/inside.gd
+++ Scene/inside.gd
@@ -1,5 +1,7 @@
 extends LevelParent
 
+@export var outside_scene: PackedScene
+
 func _on_area_2d_body_entered(_body):
 	var tween = get_tree().create_tween()
 	tween.set_parallel(true)
@@ -25,3 +27,9 @@ func _on_door_2_player_connected(_body):
 	tween.set_parallel(true)
 	tween.tween_property($Ground/door2, "position", Vector2(14042, 328), 0.5)
 	tween.tween_property($Ground/door2, "rotation", deg_to_rad(-165), 0.5)
+
+func _on_area_2d_2_body_entered(_body):
+	var tween = create_tween()
+	tween.tween_property($Player, "speed", 0, 0.5)
+	get_tree().change_scene_to_packed(outside_scene)
+
--- Scene/outside.gd
+++ Scene/outside.gd
@@ -1,5 +1,6 @@
 extends LevelParent
- 
+
+
 func _on_house_player_entered():
 	var tween = get_tree().create_tween()
 	tween.set_parallel(true)
@@ -11,3 +12,12 @@ func _on_house_player_exited():
 	tween.set_parallel(true)
 	tween.tween_property($Player/Camera2D, "zoom", Vector2(0.6,0.6), 2)
 	tween.tween_property($Player, "modulate:a", 1, 2)
+
+func _on_door_player_connected(_body):
+	pass # Replace with function body.
+
+func _on_area_2d_body_entered(_body):
+	var tween = create_tween()
+	tween.tween_property($Player, "speed", 0, 0.5)
+	get_tree().change_scene_to_file("res://Scene/inside.tscn")
+	
